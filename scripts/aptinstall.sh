1c85570712c2085e0aae51a07929bbaa76347c8e
michaelhaaf <michael.haaf@gmail.com>
2022-05-15 14:39:24 -0400

    package scripts touch-up

--- scripts/aptinstall.sh
+++ scripts/aptinstall.sh
@@ -5,59 +5,19 @@ sudo add-apt-repository ppa:mozillateam/firefox-next -y
 sudo apt update
 
 function install {
-  if [ "${1}" -ne 0 ]; then
     echo "Installing: ${1}..."
     sudo apt install -y "${1}"
-  else
-    echo "Already installed: ${1}"
-  fi
 }
 
 function custom_install {
   [[ "${1}" -ne 0 ]] && echo "Installing: ${1}" && exec "${2}" || echo "Already installed. ${1}" 
 }
 
-install curl
-install git
-install htop
-install vim
-install exa
-install calcurse
-
-install kitty
-install zbar-tools
-install shellcheck
-install pass
-install pass-extension-otp
-
-install ranger
-install calc
-install xdotool
-
-install firefox
-install chromium-browser
-install lynx
-install newsboat
-
-install python2
-install python3-venv
-install python3-pip
-install npm
-install jq
-
-install bspwm
-install lxappearance
-install rsync
-install golang
-install rofi
-install zathura
-install mpv
-install sxiv
-
-install pavucontrol
-install pulsemixer
-install redshift
-install cargo
-
-# experimental
-custom_install flavours "cargo install --locked flavours"
+# from stdin
+while read -r program options; do
+  if [[ -z "${options}" ]]; then
+    install "${program}"
+  else
+    custom_install "${program}" "${options}"
+  fi
+done
