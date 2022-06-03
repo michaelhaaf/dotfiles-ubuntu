d70cc9512c3694b32e07259f32c53f939c3ad78d
michaelhaaf <michael.haaf@gmail.com>
2022-06-03 19:48:55 -0400

    fix nvim-tree migration issue https://github.com/kyazdani42/nvim-tree.lua/issues/674

--- .config/nvim/lua/plugin_config/nvim-tree.lua
+++ .config/nvim/lua/plugin_config/nvim-tree.lua
@@ -1,29 +1,3 @@
-vim.cmd([[
-let g:nvim_tree_icons = {
-    \ 'default': '',
-    \ 'symlink': '',
-    \ 'git': {
-    \   'unstaged': "✗",
-    \   'staged': "✓",
-    \   'unmerged': "",
-    \   'renamed': "➜",
-    \   'untracked': "★",
-    \   'deleted': "",
-    \   'ignored': "◌"
-    \   },
-    \ 'folder': {
-    \   'arrow_open': "",
-    \   'arrow_closed': "",
-    \   'default': "",
-    \   'open': "",
-    \   'empty': "",
-    \   'empty_open': "",
-    \   'symlink': "",
-    \   'symlink_open': "",
-    \   }
-    \ }
-]])
-
 require 'nvim-tree'.setup {
   hijack_cursor = true,
   filters = {
